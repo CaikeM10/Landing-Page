4f293d08d27db2aee71ad967ad07e10efacb4d21
Caike Marinho <caikemarinho150@gmail.com>
2025-08-05 09:16:02 -0300

    cm1

--- src/pages/thankYou.tsx
+++ src/pages/thankYou.tsx
@@ -11,7 +11,7 @@ export default function ThankYou() {
         <p>Recebemos seu formulário com sucesso. Entraremos em contato!</p>
         <button onClick={() => router.push("https://wa.me/5588999172635")}>
           Falar no Whatsapp
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+          <svg xmlns="http://www.w3.org/2000/s  vg" viewBox="0 0 256 256">
             <rect width="256" height="256" fill="none" />
             <path
               d="M72,104a32,32,0,0,1,32-32l16,32-12.32,18.47a48.19,48.19,0,0,0,25.85,25.85L152,136l32,16a32,32,0,0,1-32,32A80,80,0,0,1,72,104Z"
