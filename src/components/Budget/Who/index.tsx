e214d8923192c1d818683a0e88f0c8bbe09212ab
Caike Marinho <caikemarinho150@gmail.com>
2025-08-08 08:38:00 -0300

    finais

--- src/components/Budget/Who/index.tsx
+++ src/components/Budget/Who/index.tsx
@@ -17,7 +17,7 @@ export default function Who() {
             <section className={styles.container}>
                 <div className={styles.content}>
                     <div className={styles.images}>
-                        <img src="/saudedigital.jpg" alt="three" />
+                        <img src="/atualizadoBanner.png" alt="three" />
                         <img src="/saudedigital.jpg" alt="one" />
                     </div>
                     <div className={styles.text}>
