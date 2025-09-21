40a4e24bd2cee9bd9912c3c687c10f897197bafc
Caike Marinho <caikemarinho150@gmail.com>
2025-09-21 11:48:15 -0300

    tags revisadas ADS e ANALYTICS

--- src/pages/thankYou.tsx
+++ src/pages/thankYou.tsx
@@ -10,10 +10,25 @@ const ThankYouPage = () => {
           content="Sua solicitação de orçamento foi enviada com sucesso. Em breve entraremos em contato."
         />
 
-        {/* --- CÓDIGO DA TAG DE CONVERSÃO DO GOOGLE ADS --- */}
+        {/* --- CÓDIGO UNIFICADO GOOGLE ADS E ANALYTICS --- */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17556512941"
+        ></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              // Configuração para o Google Ads
+              gtag('config', 'AW-17556512941');
+              
+              // Configuração para o Google Analytics
+              gtag('config', 'G-3H9BSCS7WX');
+
+              // Evento de conversão (envio de formulário)
               gtag('event', 'conversion', {'send_to': 'AW-17556512941/hLvjCKeG9ZsbEK3BzLNB'});
             `,
           }}
